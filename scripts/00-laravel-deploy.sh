ce348388b6e5caa3ec4e65fc05487a727f151d93
moroken700 <unconfigured@null.spigotmc.org>
2024-02-14 15:21:49 +0900

    delete code to confirm composer version

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 echo "Running composer"
 
-# Composerのバージョンを取得し、最初の数字（メジャーバージョン）だけを抽出
-composer_version=$(composer --version | grep -oP '^Composer version \K[0-9]+')
-
-echo "Detected Composer Version: $composer_version"
-
 composer install --no-dev --working-dir=/var/www/html
 
 echo "Caching config..."
