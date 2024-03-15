fd15aab55331fe80ba2c2ea87dc2cfc5c99113e5
moroken700 <unconfigured@null.spigotmc.org>
2024-03-15 19:50:45 +0900

    change top page 2

--- app/Http/Controllers/ProfilePage/IndexController.php
+++ app/Http/Controllers/ProfilePage/IndexController.php
@@ -12,6 +12,6 @@ class IndexController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return view('profilePage.index');
+        return view('ProfilePage.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('user.register');
+    return view('ProfilePage.index');
 });
 
 Route::get('/dashboard', function () {
